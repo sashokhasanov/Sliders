a8ba926a9f09b0bc4683e66e7f7551c55b355a09
Aleksandr Khasanov <sashokhasanov@mail.ru>
2021-12-10 16:24:54 +0300

    - edited color value label width. set to fixed. - implemented app logic

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -9,11 +9,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 20
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+    }
+    
+    override func viewWillLayoutSubviews() {
+        updateColorLabel(.red)
+        updateColorLabel(.green)
+        updateColorLabel(.blue)
+        updateColorView()
     }
 
+    @IBAction func redSliderMoved() {
+        updateColorLabel(.red)
+        updateColorView()
+    }
+    
+    @IBAction func greenSliderMoved() {
+        updateColorLabel(.green)
+        updateColorView()
+    }
+    
+    @IBAction func blueSliderMoved() {
+        updateColorLabel(.blue)
+        updateColorView()
+    }
+    
+    private func updateColorLabel(_ color: Color) {
+        switch color {
+        case .red:
+            redValueLabel.text = String(round(redSlider.value * 100) / 100)
+        case .green:
+            greenValueLabel.text = String(round(greenSlider.value * 100) / 100)
+        case .blue:
+            blueValueLabel.text = String(round(blueSlider.value * 100) / 100)
+        }
+    }
+    
+    private func updateColorView() {
+        colorView.backgroundColor =
+            UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(1.0))
+    }
+}
 
+extension ViewController {
+    enum Color {
+        case red, green, blue
+    }
 }
 
